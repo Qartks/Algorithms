11b0b1590866b34001ae2707fb06f8d6f987cfbf
Kartikeya Shukla <shukla.ka@husky.neu.edu>
2017-01-28 14:40:34 -0600

    daily update

--- CoinChange.java
+++ CoinChange.java
@@ -1,7 +1,33 @@
 package com.algorithms.qartks;
 
+import java.util.Arrays;
+
 /**
  * Created by qartks on 1/27/17.
  */
 public class CoinChange {
+
+
+    public static long countWays(int[] arr, int n) {
+
+        long ways[] = new long[n+1];
+
+        ways[0] = 1;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = arr[i]; j <=n ; j++) {
+                ways[j] += ways[j - arr[i]];
+            }
+            System.out.println(Arrays.toString(ways));
+        }
+        return ways[n];
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {1, 4, 5, 7, 15};
+        int n = 15;
+
+        System.out.println(countWays(arr, n));
+    }
 }
---
+++ Knapsack01.java
@@ -0,0 +1,17 @@
+package com.algorithms.qartks;
+
+import javafx.util.Pair;
+
+/**
+ * Created by qartks on 1/27/17.
+ */
+public class Knapsack01 {
+
+    public static void main(String[] args) {
+
+        Pair<Integer, Integer> pair = new Pair<>(5, 3);
+
+        System.out.println(pair.toString());
+
+    }
+}
