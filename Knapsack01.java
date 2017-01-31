6c998cf7e0d605c2ee536147b204ffc97134106a
Kartikeya Shukla <shukla.ka@husky.neu.edu>
2017-01-31 12:43:52 -0600

    31/1

--- Knapsack01.java
+++ Knapsack01.java
@@ -9,9 +9,30 @@ public class Knapsack01 {
 
     public static void main(String[] args) {
 
-        Pair<Integer, Integer> pair = new Pair<>(5, 3);
+        int[] values = {60, 100, 120};
+        int[] weights = {10, 20, 30};
+        int W = 50;
 
-        System.out.println(pair.toString());
+        solve(values, weights, W);
 
+    }
+
+    private static void solve(int[] values, int[] weights, int W) {
+        int N = values.length;
+        int[][] dp = new int[N+1][W+1];
+
+        for (int i = 0; i <= N; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (i == 0 || w == 0) {
+                    dp[i][w] = 0;
+                } else if (weights[i-1] <= w) {
+                    dp[i][w] = Math.max(dp[i-1][w], values[i-1] + dp[i-1][w - weights[i-1]]);
+                } else {
+                    dp[i][w] = dp[i-1][w];
+                }
+            }
+        }
+
+        System.out.println(dp[N][W]);
     }
 }
